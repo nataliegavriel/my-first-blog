a0321c5227193069ade6480cc8fb5ef41a7ac43f
nataliegavriel <natalie.gavriel@gmail.com>
2016-08-20 18:29:11 +0300

    20.08.2016 commit 1

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,12 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    #file = forms.FileField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
